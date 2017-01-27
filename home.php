687a069252ecfae14980a3325b60b1a928e5e0a3
NPavao <NPavao24@gmail.com>
2017-01-27 16:45:52 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,27 @@
+<?php
+
+echo '
+<head>
+  <title>Sample Juice Truck Page</title>	
+</head>
+<body>
+
+<h1>Welcome To NUWC Juicing Co.</h1>
+
+<table border="1">
+
+<thead>
+    <tr>
+      <th> <a href="home.php">Home</a></th>
+      <th> <a href="menu.php">Menu</a></th>
+      <th> <a href="about.php">About</a></th>
+      <th> <a href="locations_contact.php">Locations & Contact Us</a></th>
+      <th> <a href="my_account.php">My Account</a></th>
+      <th> <a href="cart.php">Cart</a></th>
+      <th> <a href="login.php">Sign In / Sign Up</a></th>
+    </tr>
+</thead>    
+  
+</body>';
+
+?>
