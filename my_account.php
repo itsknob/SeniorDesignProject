8c477ab1702d46e2be1233614f71e9bbffdcb83f
NPavao <NPavao24@gmail.com>
2017-01-27 16:33:47 -0500

    Update my_account.php

--- my_account.php
+++ my_account.php
@@ -12,13 +12,13 @@
 
   <thead>
       <tr>
-        <th> <a href="index.html">Home</a></th>
-        <th> <a href="menu.html">Menu</a></th>
-        <th> <a href="about.html">About</a></th>
-        <th> <a href="locations_contact.html">Locations & Contact Us</a></th>
-        <th> <a href="my_account.html">My Account</a></th>
-        <th> <a href="cart.html">Cart</a></th>
-        <th> <a href="index.html">Sign In / Sign Up</a></th>
+        <th> <a href="home.php">Home</a></th>
+        <th> <a href="menu.php">Menu</a></th>
+        <th> <a href="about.php">About</a></th>
+        <th> <a href="locations_contact.php">Locations & Contact Us</a></th>
+        <th> <a href="my_account.php">My Account</a></th>
+        <th> <a href="cart.php">Cart</a></th>
+        <th> <a href="login.php">Sign In / Sign Up</a></th>
       </tr>
   </thead>    
   
