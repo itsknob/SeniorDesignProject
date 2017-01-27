48f828e8e202c2c6a49b63c5cd1b2913528f53bd
itsknob <itsknob@gmail.com>
2017-01-27 16:12:18 -0500

    Added Registration.php

---
+++ registration.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title> Registration Page </title>
+	</head>
+	<body>
+		<form action='registration.php' method='POST'>
+			<table width='500' border='10' align='center'>
+				<tr>
+					<td align='center' colspan='5'><h2>Registration</h2></td>
+				</tr>
+				<tr>
+					<td align='center'>Email:</td>
+					<td><input type='email' name='pass' /></td>
+				</tr>
+				<tr>
+					<td align='center'>User Name:</td>
+					<td><input type='text' name='name' /></td>
+				</tr>
+				<tr>
+					<td align='center'>Password:</td>
+					<td><input type='password' name='pass' /></td>
+				</tr>
+				<tr>
+					<td align='center' colspan='3'><input type='submit' /></td>
+					
+				</tr>
+		</form>
+	</body>
+</html>
