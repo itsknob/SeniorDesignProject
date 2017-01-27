a0278851f46f6d68547cddce5bd17a24c4581465
NPavao <NPavao24@gmail.com>
2017-01-27 16:38:33 -0500

    Update and rename cart.html to cart.php

---
+++ cart.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Cart</title>
+</head>
+<body>
+
+  <h1>Cart</h1>
+  
+<!--  Table should be done in CSS this is just a demo-->
+  <table border="1">
+
+  <thead>
+      <tr>
+        <th> <a href="home.php">Home</a></th>
+        <th> <a href="menu.php">Menu</a></th>
+        <th> <a href="about.php">About</a></th>
+        <th> <a href="locations_contact.php">Locations & Contact Us</a></th>
+        <th> <a href="my_account.php">My Account</a></th>
+        <th> <a href="cart.php">Cart</a></th>
+        <th> <a href="login.php">Sign In / Sign Up</a></th>
+      </tr>
+  </thead>    
+  
+  
+</body>  
+</html>
