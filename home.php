30ad475939d5e2aa9ae30c860be2f5041b28d4d9
palexandre1 <palexandre822@gmail.com>
2017-02-13 15:20:30 -0500

    Add files via upload
    Slideshow on homepage

--- home.php
+++ home.php
@@ -59,13 +59,58 @@ $companyName = "Company Name";
 
 		<div class="container">
 			<div class="content">
-				<p>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br>@<br></p>
+				
 			</div>
 		</div>
-		
-	
+    
+    
+<!--    Slideshow-->
+<div id="myCarousel" class="carousel slide" data-ride="carousel" style="width: 50%">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class  ="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img1.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="img2.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="img3.jpg" alt="Flower">
+    </div>
+
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    
+<!--End Slideshow   -->
+    
+    
+    
+<!--Twitter Widget-->
+<div class="pull-right">
 	<a class="twitter-timeline" href="https://twitter.com/nuwcJuicing"> Tweets by nuwcJuicing </a>
 	<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-	
+</div>
+<!--End Twitter Widget-->
+    
 	</body>
 </html>
