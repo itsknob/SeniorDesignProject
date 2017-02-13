7dc2cc3f6990bf354e0a3562588a9761d570a350
atavares2 <ajtavares2@gmail.com>
2017-02-13 17:29:12 -0500

    Touched up home.php and styles.css
    Sectionalized the deal of the day, popular items, twitter feed
    horizontally, added slideshow to the bottom

--- home.php
+++ home.php
@@ -15,11 +15,11 @@ $companyName = "Company Name";
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <html>
-	<head>
-		<title>Sample Juice Truck Page</title>
-		<link rel="stylesheet" type"text/css" href="styles.css">
-	</head>
-	<body>
+<head>
+	<title>Sample Juice Truck Page</title>
+	<link rel="stylesheet" type"text/css" href="styles.css">
+</head>
+<body>
 
 	<!-- Tried to get the navbar to change if the user is logged in. It doesnt work though. -->
 	<?php
@@ -29,88 +29,120 @@ $companyName = "Company Name";
 		include 'navbar_unauthorized.php';
 	}
 	?>
-		<!-- Once the php code above works this can be deleted -->
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-		 	<div class="container-fluid">
-		    	<div class="navbar-header">
-		     		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-		        		<span class="icon-bar"></span>
-		        		<span class="icon-bar"></span>
-		        		<span class="icon-bar"></span> 
-		      		</button>
-		    		<a class="navbar-brand" href="home.php">Juice Company</a>
-		    	</div>
-		    	<div class="collapse navbar-collapse" id="myNavbar">
-		      		<ul class="nav navbar-nav">
-		        		<li class="active"><a href="home.php">Home</a></li>
-		        		<li><a href="menu.php">Menu</a></li>
-		        		<li><a href="about.php">About Us</a></li> 
-		        		<li><a href="locations_contact.php">Locations & Contact Us</a></li> 
-		      		</ul>
-		      		<ul class="nav navbar-nav navbar-right">
-		        		<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-		        		<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-		        		<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
-		      		</ul>
-		    	</div>
-		  	</div>
-		</nav>
-	
-
-		<div class="container">
-			<div class="content">
-				
+	<!-- Once the php code above works this can be deleted -->
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span> 
+				</button>
+				<a class="navbar-brand" href="home.php">Juice Company</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="home.php">Home</a></li>
+					<li><a href="menu.php">Menu</a></li>
+					<li><a href="about.php">About Us</a></li> 
+					<li><a href="locations_contact.php">Locations & Contact Us</a></li> 
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+				</ul>
 			</div>
 		</div>
-    
-    
-<!--    Slideshow-->
-<div id="myCarousel" class="carousel slide" data-ride="carousel" style="width: 50%">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-    <li data-target="#myCarousel" data-slide-to="3"></li>
-  </ol>
+	</div>
+	
+	<div class="main">
+		<!-- Most Popular Menu Items -->
+		<div class="left well">
+			<table class="table">
+				<thead>
+					<th>
+						Popular Items
+					</th>
+					<tr>
+						<th>#</th>
+						<th>Item</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Apple Juice</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Orange Juice</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>Other Juice</td>
+					</tr>
+					<tr>
+						<td>4</td>
+						<td>Other Juice</td>
+					</tr>
+					<tr>
+						<td>5</td>
+						<td>Other other Juice</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 
-  <!-- Wrapper for slides -->
-  <div class  ="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="img1.jpg" alt="Chania">
-    </div>
+		<div class="middle">
+			Deal of the day goes here
+			<br>k
+		</div>
+		
+		<div class="right">
+			<a class="twitter-timeline" href="https://twitter.com/nuwcJuicing"> Tweets by nuwcJuicing </a>
+			<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+		</div>
+		
+	</div>
 
-    <div class="item">
-      <img src="img2.jpg" alt="Chania">
-    </div>
 
-    <div class="item">
-      <img src="img3.jpg" alt="Flower">
-    </div>
+	<!--    Slideshow-->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel" style="width: 50%">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+			<li data-target="#myCarousel" data-slide-to="3"></li>
+		</ol>
 
-  </div>
+		<!-- Wrapper for slides -->
+		<div class  ="carousel-inner" role="listbox">
+			<div class="item active">
+				<img src="img1.jpg" alt="Chania">
+			</div>
 
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-    
-<!--End Slideshow   -->
-    
-    
-    
-<!--Twitter Widget-->
-<div class="pull-right">
-	<a class="twitter-timeline" href="https://twitter.com/nuwcJuicing"> Tweets by nuwcJuicing </a>
-	<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-</div>
-<!--End Twitter Widget-->
-    
-	</body>
+			<div class="item">
+				<img src="img2.jpg" alt="Chania">
+			</div>
+
+			<div class="item">
+				<img src="img3.jpg" alt="Flower">
+			</div>
+
+		</div>
+
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+	<!--End Slideshow   -->
+</body>
 </html>
