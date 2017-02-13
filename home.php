553486a886289b5c981d281eb4e36b843067e799
itsknob <itsknob@gmail.com>
2017-02-13 18:00:59 -0500

    Fixed Links in home.php

--- home.php
+++ home.php
@@ -48,8 +48,8 @@ $companyName = "Company Name";
 					<li><a href="locations_contact.php">Locations & Contact Us</a></li> 
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+					<li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+					<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
 					<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
 				</ul>
 			</div>
