86d611f97e80497a984c960b225c9ebd6cdc06ec
itsknob <itsknob@gmail.com>
2017-03-06 15:28:07 -0500

    fixed home and styles

--- home.php
+++ home.php
@@ -124,15 +124,15 @@
 		<!-- Wrapper for slides -->
 		<div class  ="carousel-inner" role="listbox">
 			<div class="item active">
-				<img src="img1.png" alt="Chania">
+				<img src="images/img1.png" alt="Chania">
 			</div>
 
 			<div class="item">
-				<img src="img2.jpg" alt="Chania">
+				<img src="images/img2.png" alt="Chania">
 			</div>
 
 			<div class="item">
-				<img src="img3.jpg" alt="Flower">
+				<img src="images/img3.jpg" alt="Flower">
 			</div>
 
 		</div>
