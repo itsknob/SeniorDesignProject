09b540ebea30866745be06f01b05a8d55646c634
Stephen C Reilly <sreilly@umassd.edu>
2017-03-07 12:13:24 -0500

    Made Registration Pretty

--- registration.php
+++ registration.php
@@ -1,6 +1,15 @@
 <html>
 	<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<title> Registration Page </title>
+		<link rel="stylesheet" type"text/css" href="styles.css">
 	</head>
 	<body>
 	
