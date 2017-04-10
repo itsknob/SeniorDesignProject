7c9c17d1fa76f4aecb5b0d7206a3da9d1a3a417a
NPavao <NPavao24@gmail.com>
2017-04-09 22:15:07 -0400

    Add files via upload

--- menu.php
+++ menu.php
@@ -9,48 +9,16 @@
 	error_reporting(E_ALL);
 	//Used to set session information
 	include "scripts.php";
+    include "showItems.php";
 	//Variables
 	$dbhost = "localhost";
 	$dbuser = "root";
-	$dbpass = "root";
-	$dbname = "inventory";
-	//Class to hold item data from database
-	class Item{
-		var $itemID;
-		var $name;
-		var $price;
-		var $desc;
-		var $cals;
-		var $prot;
-		var $chol;
-		var $sodi;
-		var $picture;
-		
-		public function Item($id, $na, $pr, $de, $ca, $pt, $ch, $so, $pi){
-			$this->itemID = $id;
-			$this->name = $na;
-			$this->price = $pr;
-			$this->desc = $de;
-			$this->cals = $ca;
-			$this->prot = $pt;
-			$this->chol = $ch;
-			$this->sodi = $so;
-			$this->picture = $pi;
-		}
-	}
-	//This array holds items from database.
-	$itemList = array();
-	//Connect and Select	
-	$con = makeConnection($dbhost, $dbuser, $dbpass, $dbname);
-	$query = "SELECT * FROM items";
-	$result = mysqli_query($con, $query);
-	
-	//Collect data from all items
-	while($row = $result->fetch_assoc())
-	{
-		$tempItem = new Item($row['itemID'], $row['itemName'],  $row['price'], $row['description'], $row['calories'], $row['protein'], $row['choles'], $row['sodi'], $row['picLink']);
-		$itemList[] = $tempItem;
-	}
+	$dbpass = "";
+	$dbname = "juicing";
+  
+    $itemList = $db->prepare('SELECT * FROM items');
+    $itemList->execute();
+    $itemList = $itemList->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!-- Latest compiled and minified CSS -->
@@ -67,8 +35,56 @@
 		<title>Menu Page</title>
 		<link rel="stylesheet" type="text/css" href="styles.css">
 		<link rel="javascript" type="text/javascript" href="scripts/scripts.js">
+		
+    <style>
+.dropbtn {
+    background-color: #4CAF50;
+    color: white;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+    cursor: pointer;
+}
+.dropbtn:hover, .dropbtn:focus {
+    background-color: #3e8e41;
+}
+#myInput {
+    border-box: box-sizing;
+    background-image: url('searchicon.png');
+    background-position: 14px 12px;
+    background-repeat: no-repeat;
+    font-size: 16px;
+    padding: 14px 20px 12px 45px;
+    border: none;
+}
+.dropdown {
+    float: right;
+    position: relative;
+    display: inline-block;
+}
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f6f6f6;
+    min-width: 230px;
+    overflow: auto;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    right: 0;
+    z-index: 1;
+}
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+.dropdown a:hover {background-color: #ddd}
+.show {display:block;}
+</style>
+
 	</head>
 	<body>
+		<!-- Once the php code above works this can be deleted -->
 		<nav class="navbar navbar-inverse navbar-fixed-top">
 		 	<div class="container-fluid">
 		    	<div class="navbar-header">
@@ -107,17 +123,93 @@
 		    	</div>
 		  	</div>
 		</nav>
-
 		<div id="menu-content" align="center">
-			<h2>Menu</h2>
-			<div id="menu-item-container" align="center">
+			<h2>Welcome to the Menu Page</h2>
+		
+		<div class="main">
+		<!--      Checkboxes-->
+		  <div class="checkboxes">
+			  <!--    Calories-->
+			<div class="calories-filter">
+			<p><strong>Filter by Calories</strong></p>
+			
+			<form>
+			  <label><input type="checkbox" id="box1" class="calories" value="300">Less than 300</label><br>
+			  <label><input type="checkbox" class="calories" value="500">Less than 500</label><br>    
+			</form>
+			</div>
+			
+			  <!--    Sugar-->
+		  <div class="sugar-filter">
+			<p><strong>Filter by Sugar (g)</strong></p>
+			
+			<form>
+			  <label><input type="checkbox" class="sugars" value="5">Less  than 5</label><br>
+			  <label><input type="checkbox" class="sugars" value="10">Less than  10</label><br>    
+			</form>
+		  </div>    
+		<!--      Prices-->
+		  <div class="price-filter">
+			<p><strong>Filter by Price</strong></p>
+			
+			<form>
+			  <label><input type="checkbox" class="price" value="5">Less  than $5</label><br>
+			  <label><input type="checkbox" class="price" value="10">Less than  $10</label><br>    
+			</form>
+			</div>
+		</div>  
+	  
+		<div id="menu-item-container" align="center">
 				<!-- REQUIRED FOR SCRIPTS TO POPULATE MENU -->
 				<!-- 			 DO NOT DELETE 	 		   -->
-			</div>
-			<script src="scripts/scripts.js">
-			</script>
-			<script> 
-				var newItemList = [];
+		</div>
+		</div>
+     
+    
+ 
+      
+      
+	<script src="scripts/scripts.js"> </script>  
+    <script>
+		var newItemList = [];
+		var allItemsArray = <?php echo json_encode($itemList); ?>;
+		//console.log("AllItemArray: " + allItemsArray);
+		//Convert PHP Items to Javascript Items
+			for(var i = 0; i < allItemsArray.length; i++){
+				var tempItem = new ItemJS();
+				var object = allItemsArray[i];
+				//console.log(object);
+				tempItem.name = object.itemName;
+				tempItem.picture = object.picLink;
+				tempItem.description = object.description;
+				tempItem.cost = object.price;
+			
+				//Save to Array at index [i]
+				newItemList[i] = tempItem;
+			}
+			console.log(allItemsArray);
+			
+			newItemList.forEach(function (d){
+				var div = document.createElement('div');
+				div.setAttribute("class", "itemcontainer");
+				document.getElementById('menu-item-container').appendChild(div);
+			});
+		/*
+		//Call function to format data
+		var container = (createTableFormattedListOfItems(newItemList));    
+		document.getElementById('menu-item-container').appendChild(container);	//Was itemArray
+		*/
+		</script>    
+      
+      
+    <script>             
+    //If checkbox is checked send a request to run a query on the database FROM items WHERE .class '<' this.value then echo the results
+      $(":checkbox").on("change",function() {
+    
+        //If no checkboxes are checked display all menu items
+    if($("input:checked" ).length == 0){
+      
+      var newItemList = [];
 				var allItemsArray = <?php echo json_encode($itemList); ?>;
 				//console.log("AllItemArray: " + allItemsArray);
 				//Convert PHP Items to Javascript Items
@@ -125,9 +217,9 @@
 					var tempItem = new ItemJS();
 					var object = allItemsArray[i];
 					//console.log(object);
-					tempItem.name = object.name;
-					tempItem.picture = object.picture;
-					tempItem.description = object.desc;
+					tempItem.name = object.itemName;
+					tempItem.picture = object.picLink;
+					tempItem.description = object.description;
 					tempItem.cost = object.price;
 				
 					//Save to Array at index [i]
@@ -135,8 +227,68 @@
 				}
 		
 				//Call function to format data
-				document.getElementById('menu-item-container').appendChild(createTableFormattedListOfItems(newItemList));	//Was itemArray
-			</script>
+      container = (createTableFormattedListOfItems(newItemList)); 
+      $('#menu-item-container').empty().append(container);
+    }  
+      
+      //runs code in reference to selected checkbox  
+      if (this.checked) {
+        let boxClass = $(this).attr('class');
+        let boxValue = $(this).attr('value');
+        //let caloriesValue = $(this).attr('value');
+        console.log(boxClass);
+        $.ajax({
+          type: 'POST',
+          url: 'showItems.php',
+          data: { class: boxClass, value: boxValue },
+          dataType: 'json',
+         error: function(xhr,textStatus,err)
+          {
+            console.log("readyState: " + xhr.readyState);
+            console.log("responseText: "+ xhr.responseText);
+            console.log("status: " + xhr.status);
+            console.log("text status: " + textStatus);
+            console.log("error: " + err);
+          },
+          success: function(result) {
+            console.log("hello world");
+            console.log(result);
+            //console.log(JSON.parse(result));
+          // here is the code that will run on client side after running showItems.php on server
+                  
+                  
+        var newItemList = [];
+				//var allItemsArray = filteredList;
+				//console.log("AllItemArray: " + allItemsArray);
+				//Convert PHP Items to Javascript Items
+				for(var i = 0; i < result.length; i++){
+					var tempItem = new ItemJS();
+					var object = result[i];
+					//console.log(object);
+					tempItem.name = object.itemName;
+					tempItem.picture = object.picLink;
+					tempItem.description = object.description;
+					tempItem.cost = object.price;
+				
+					//Save to Array at index [i]
+					newItemList[i] = tempItem;
+				}
+		    
+				//Call function to format data
+        container = (createTableFormattedListOfItems(newItemList));    
+            
+//				document.getElementById('menu-item-container').appendChild(container);	//Was itemArray
+        
+         $('#menu-item-container').empty().append(container);
+    
+        // function below reloads current page
+        //location.reload();
+              }
+          });
+      }        
+  });     
+    </script>  
+      
 		</div>
 	</body>
 </html>
