d33636d70feeeb1c43d62d082b630f250af3642b
palexandre1 <palexandre822@gmail.com>
2017-04-17 18:23:31 -0400

    Add files via upload
    sends over an array of items

--- fetch.php
+++ fetch.php
@@ -7,54 +7,26 @@
   $dbname = "juicing";
 
   $output = '';
+  $search = $_POST["query"];
 
-  $connect = mysqli_connect("localhost", "root", "", "juicing");
+	$db = new PDO('mysql:host=localhost;dbname=juicing;charset=utf8', 'root', '');
   if(isset($_POST["query"]))
   {
-    $search = mysqli_real_escape_string($connect, $_POST["query"]);
-    $query = "
+    
+    $items = $db->prepare("
       SELECT * FROM items 
       WHERE itemName LIKE '%".$search."%'
       OR price LIKE '%".$search."%' 
       OR description LIKE '%".$search."%' 
       OR calories LIKE '%".$search."%' 
       OR protein LIKE '%".$search."%'
-    ";
+    ");
+    $items->execute();
+    $items = $items->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($items);
     }
   else
   {
     return;
-  }
-  $result = mysqli_query($connect, $query);
-  if(mysqli_num_rows($result) > 0)
-  {
-    $output .= '
-    <div class="table-responsive">
-    <table class="table table bordered">
-      <tr>
-      <th>Drink Name</th>
-      <th>Price</th>
-      <th>Description</th>
-      <th>Calories</th>
-      <th>Protein</th>
-      </tr>
-  ';
-  while($row = mysqli_fetch_array($result))
-  {
-    $output .= '
-    <tr>
-      <td>'.$row["itemName"].'</td>
-      <td>'.$row["price"].'</td>
-      <td>'.$row["description"].'</td>
-      <td>'.$row["calories"].'</td>
-      <td>'.$row["protein"].'</td>
-    </tr>
-    ';
-  }
-  echo $output;
-  }
-  else
-  {
-    echo 'Data Not Found';
   }
 ?>
